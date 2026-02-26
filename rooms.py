f9cbe09e0028c5bddbe280b7bfadc4069d1546bc
mcmcpiece <mcmcpiece@gmail.com>
2026-02-26 11:00:12 +0800

    initial push

---
+++ rooms.py
@@ -0,0 +1,52 @@
+rooms = {
+    "lobby": {
+        "description": "You are in a grand hall with a chandelier hanging from the ceiling. Shockingly, it is empty. You see an arched doorway to four directions.",
+        "directions": {
+            "north" : "treasury",
+            "south" : "library",
+            "east" : "reserved armory",
+            "west" : "statue"
+        },
+        "items" : ["pamphlet", "key"]
+},
+    "treasury": {
+        "description" : "You are in a room with chests full of gold and jewels.",
+        "directions" : {
+            "north" : "nothing",
+            "south" : "lobby",
+            "east" : "nothing",
+            "west" : "nothing"
+    },
+        "items" : ["bag of gold", "ruby necklace"]   
+},
+    "library" : {
+        "description" : "There is a huge collection of books here.",
+        "directions" : {
+            "north" : "lobby",
+            "south" : "nothing",
+            "east" : "door",
+            "west" : "nothing"
+        },
+        "items" : ["red scroll"]
+    },
+    "reserved armory" : {
+        "description" : "You see a collection of weapons and armor. As you look around, you cannot avoid feeling unease remembering how each of these weapons were used in battle.",
+        "directions" : {
+            "north" : "nothing",
+            "south" : "nothing",
+            "east" : "nothing",
+            "west" : "lobby"
+        },
+        "items" : [""]
+    },
+    "statue" : {
+        "description" : "There is a statue of King Medhias here. It looks like it has something in its hand. When you checked closer, you see that it is a silver dagger",
+        "directions" : {
+            "north" : "nothing",
+            "south" : "nothing",
+            "east" : "lobby",
+            "west" : "nothing"
+        },
+        "items" : ["silver dagger"]
+    }
+}
---
+++ rpg.py
@@ -0,0 +1,75 @@
+#Text based game; only meant to exercise my programming skills in python
+import rooms
+
+current_room = "lobby"
+inventory = []
+
+
+def intro():
+    print(f'''
+Welcome, adventurer! You are in the {current_room}. Decide your move!
+=====================================================================
+Commands:
+go [direction]
+get [item]
+check
+use [object]
+''')
+    
+def go(direction):
+    global current_room
+    if direction in rooms.rooms[current_room]["directions"]:
+        if rooms.rooms[current_room]["directions"][direction] == "nothing":
+            print("You can't go that way.")
+            return
+        else:
+            current_room = rooms.rooms[current_room]["directions"][direction]
+            print(f"You move to the {current_room}.")
+    else:
+        print("You can't go that way.")
+
+def getitem(item):
+    global inventory
+    if item in rooms.rooms[current_room]["items"]:
+        print(f"You pick up the {item}.")
+        inventory.append(item)
+        rooms.rooms[current_room]["items"].remove(item)
+    else:
+        print(f"There is no {item} here.")
+
+def main():
+    intro()
+    while True:
+        move = input(">").split(" ", 1)
+        action = move[0]
+        get_currentroom = rooms.rooms[current_room]
+        if action == "go":
+            direction = move[1]
+            go(direction)
+        elif action == "get":
+            item = move[1]
+            getitem(item)
+        elif action == "check":
+            print(f"You check your surroundings. {get_currentroom["description"]}")
+        else:
+            print("I did not quite understand that command. Please try again.")
+            print('''
+Commands:
+go [direction]
+get [item]
+check
+use [object]
+''')
+
+def list_items():
+    for i in rooms.rooms:
+        for item in rooms.rooms[i]["items"]:
+            print(item)
+
+list_items()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("\nDid you mean to exit? Exiting the game...")
